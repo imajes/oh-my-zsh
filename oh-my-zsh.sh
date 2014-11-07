8b68807896e89015cd889567f839c03f23226aa4
James Cox <james@imaj.es>
2014-11-06 23:22:53 -0500

    ohmyzsh version

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -6,3 +6,8 @@ for config_file ($ZSH/lib/*.zsh) source $config_file
 
 # Load all of your custom configurations from custom/
 for config_file ($ZSH/custom/*.zsh) source $config_file
+
+# set a version so we know where we are. hopefully this means
+# 3rd parties such as rvm will know we support them out of the 
+# box, and won't try and install if they see us
+export OH_MY_ZSH=0.0.5
